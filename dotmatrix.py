2f13153b6a9face75ff945153455c6d6567e77d5
Gustavo León <gustavo.leon.paredes@gmail.com>
2020-06-09 02:56:50 -0500

    arreglos gráfico

--- dotmatrix.py
+++ dotmatrix.py
@@ -1,17 +1,17 @@
-import pylab
+import matplotlib.pyplot as plt
 from Bio import SeqIO
 
 
 def dot_matrix(file, window, threshold):
     in_handle = open(file)
     record_iterator = SeqIO.parse(in_handle, "fasta")
-    rec_one = next(record_iterator)
-    rec_two = next(record_iterator)
-    dict_one = {}
-    dict_two = {}
+    rec_1 = next(record_iterator)
+    rec_2 = next(record_iterator)
+    chunks_1 = {}
+    chunks_2 = {}
     for (seq, section_dict) in [
-        (str(rec_one.seq).upper(), dict_one),
-        (str(rec_two.seq).upper(), dict_two),
+        (str(rec_1.seq).upper(), chunks_1),
+        (str(rec_2.seq).upper(), chunks_2),
     ]:
         for i in range(len(seq) - window):
             section = seq[i: i + window]
@@ -20,27 +20,18 @@ def dot_matrix(file, window, threshold):
             except KeyError:
                 section_dict[section] = [i]
 
-    matches = set(dict_one).intersection(dict_two)
+    matches = set(chunks_1).intersection(chunks_2)
     x = []
     y = []
     for section in matches:
-        for i in dict_one[section]:
-            for j in dict_two[section]:
+        for i in chunks_1[section]:
+            for j in chunks_2[section]:
                 x.append(i)
                 y.append(j)
-
-    pylab.cla()
-    pylab.gray()
-    pylab.scatter(x, y)
-    pylab.xlim(0, len(rec_one) - window)
-    pylab.ylim(0, len(rec_two) - window)
-    pylab.xlabel("%s (length %i bp)" % (rec_one.id, len(rec_one)))
-    pylab.ylabel("%s (length %i bp)" % (rec_two.id, len(rec_two)))
-    pylab.title("DotMatrix FLNA  Window:%s)" % window)
-    pylab.show()
-
-    # for char1 in data_1:
-    #     print(char1)
+    plt.scatter(x, y)
+    plt.xlabel("%s (length %i bp)" % (rec_1.id, len(rec_1)))
+    plt.ylabel("%s (length %i bp)" % (rec_2.id, len(rec_2)))
+    plt.show()
 
 
 dot_matrix('flna.fasta', 5, 20)
