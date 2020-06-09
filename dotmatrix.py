5f9587cb1957109fc7969c39865ec4d9d66836c2
Gustavo León <gustavo.leon.paredes@gmail.com>
2020-06-09 06:41:14 -0500

    añadida validación de threshold

--- dotmatrix.py
+++ dotmatrix.py
@@ -3,7 +3,7 @@ from Bio import SeqIO
 
 
 def identity(win_1, win_2, threshold):
-    res = len(set(win_1) & set(win_2)) / float(len(set(win_1) | set(win_2))) * 100
+    res = len(set(win_1) & set(win_2)) / max(len(win_1), len(win_2)) * 100
     # print(res)
     if res >= threshold:
         return True
@@ -25,14 +25,15 @@ def dot_matrix(file, window, threshold):
         for j in range(len(chunks_2)):
             if identity(chunks_1[i], chunks_2[j], threshold):
                 x.extend([n for n in range(i*window, (i*window)+window)])
-                y.extend([n for n in range(j*window, (j*window) + window)])
+                y.extend([n for n in range(j*window, (j*window)+window)])
 
     plt.scatter(x, y)
     plt.xlabel("%s (length %i bp)" % (rec_1.id, len(rec_1)))
     plt.ylabel("%s (length %i bp)" % (rec_2.id, len(rec_2)))
+    plt.title("window: %s | threshold: %s" % (window, threshold))
     plt.show()
 
 
-dot_matrix('flna.fasta', 75, 100)
-# print(identity('ACT', 'ATC', 100))
+dot_matrix('flna.fasta', 25, 50)
+# print(identity('MSSSH', 'GSASC', 21))
 
